91575c947f7a943029b9ee827ea65b0badb9bee7
rithwik krishna <rithwikkrishna0002@gmail.com>
2023-02-11 20:14:54 +0530

    initial commit

---
+++ test.js
@@ -0,0 +1,63 @@
+// const a = 10;
+// console.log(a);
+
+// const dd = () => {
+// const a = 20
+// console.log(a)
+// }
+
+// dd()
+
+// const obj = {
+//     fn: function demo() {
+//         console.log(this);
+//     },
+//     fn2: function() {
+//         console.log(this);
+//     }
+// }
+
+// obj.fn2(); 
+
+// function x() {
+// function y(){
+// console.log(this);
+// }
+// y()
+// }  
+// x()
+
+// const person = {
+//     name: 'akshay'
+// }
+
+// const person2 = {
+//     name : 'yuku'
+// }
+
+// function x() {
+//     console.log(this);
+//     console.log("in");
+// }
+
+// x.call(person)
+// x.call(this)
+ 
+
+// const obj={
+//     name:'indhu',
+//     print: function() {
+//         console.log(this,"first");
+//     }
+//     }
+//     obj.print()
+    
+//     const obj2={
+//         name:'rtwk',
+//         print: ()=> {
+//             console.log(this,"secnd");
+//         }
+//     }
+    
+//     obj2.print()
+    
